baa6ff9010eaf795d4e98cfed68b29a9eea1307b
verynicetomato <155400101+verynicetomato@users.noreply.github.com>
2024-02-15 22:06:58 +0900

    Add files via upload

---
+++ Python_Introduction/dict_loop.py
@@ -0,0 +1,11 @@
+
+ages = {'Tod':35, 'Jane':23, 'Paul':62}
+
+for key in ages.keys():
+	print('{}의 나이는 {}입니다.'.format(key, ages[key]))
+	
+# for key, value in ages.items():
+#   print('{}의 나이는 {}입니다.'.format(key, value))
+ 
+ 
+	
