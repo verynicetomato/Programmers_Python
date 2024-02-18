9c5c79da0d2659758100999c223f78026d1ab8df
verynicetomato <155400101+verynicetomato@users.noreply.github.com>
2024-02-18 19:45:49 +0900

    Add files via upload

---
+++ LEVEL 1/짝수와 홀수.py
@@ -0,0 +1,9 @@
+# LEVEL 1: 짝수와 홀수
+
+def solution(num):
+    answer = ''
+    if num % 2 == 0:
+        answer = 'Even'
+    else:
+        answer = 'Odd'
+    return answer
---
+++ LEVEL 1/평균 구하기.py
@@ -0,0 +1,8 @@
+# LEVEL 1: 평균 구하기
+
+def solution(arr):
+    answer = 0
+    for i in arr:
+        answer += i
+    answer = answer / len(arr)
+    return answer
