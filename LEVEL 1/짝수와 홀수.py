2ea264dd20be14b2ea8933f8bb94258626e75c0d
verynicetomato <155400101+verynicetomato@users.noreply.github.com>
2024-02-18 19:46:18 +0900

    Delete LEVEL 1 directory

--- LEVEL 1/짝수와 홀수.py
+++
@@ -1,9 +0,0 @@
-# LEVEL 1: 짝수와 홀수
-
-def solution(num):
-    answer = ''
-    if num % 2 == 0:
-        answer = 'Even'
-    else:
-        answer = 'Odd'
-    return answer
--- LEVEL 1/평균 구하기.py
+++
@@ -1,8 +0,0 @@
-# LEVEL 1: 평균 구하기
-
-def solution(arr):
-    answer = 0
-    for i in arr:
-        answer += i
-    answer = answer / len(arr)
-    return answer
